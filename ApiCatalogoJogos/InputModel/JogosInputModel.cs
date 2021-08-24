a6a924c2162ed45351384c91901c1cc909fbff17
douglashassislima <douglashenriquedeassis@gmail.com>
2021-08-24 09:50:10 -0300

    Alterando comandos dentro da classe JogosInputModel

--- ApiCatalogoJogos/InputModel/JogosInputModel.cs
+++ ApiCatalogoJogos/InputModel/JogosInputModel.cs
@@ -9,13 +9,13 @@ namespace ApiCatalogoJogos.InputModel
     public class JogosInputModel
     {
         [Required]
-        [StringLength(100, MinimumLength = 3, ErrorMessage = "O nome do jogo deve conter entre 4 e 150 caracteres")]
+        [StringLength(150, MinimumLength = 4, ErrorMessage = "O nome do jogo deve conter entre 4 e 150 caracteres")]
         public string Nome { get; set; }
         [Required]
-        [StringLength(100, MinimumLength = 1, ErrorMessage = "O nome da produtora deve conter entre 4 e 150 caracteres")]
+        [StringLength(150, MinimumLength = 4, ErrorMessage = "O nome da produtora deve conter entre 4 e 150 caracteres")]
         public string Produtora { get; set; }
         [Required]
-        [Range(1, 100, ErrorMessage = "O preço deve ser de no mínimo 5 reais e no máximo 100 reais")]
+        [Range(5, 100, ErrorMessage = "O preço deve ser de no mínimo 5 reais e no máximo 100 reais")]
         public string Preco { get; set; }
     }
 }
