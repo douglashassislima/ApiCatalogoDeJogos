4165d85bcbf080dac4b9b570eddce1b7e31ddb54
douglashassislima <douglashenriquedeassis@gmail.com>
2021-08-24 09:48:27 -0300

    Adicionando a nova classe JogosInputModel na pasta InputModel

---
+++ ApiCatalogoJogos/InputModel/JogosInputModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiCatalogoJogos.InputModel
+{
+    public class JogosInputModel
+    {
+        [Required]
+        [StringLength(100, MinimumLength = 3, ErrorMessage = "O nome do jogo deve conter entre 4 e 150 caracteres")]
+        public string Nome { get; set; }
+        [Required]
+        [StringLength(100, MinimumLength = 1, ErrorMessage = "O nome da produtora deve conter entre 4 e 150 caracteres")]
+        public string Produtora { get; set; }
+        [Required]
+        [Range(1, 100, ErrorMessage = "O preço deve ser de no mínimo 5 reais e no máximo 100 reais")]
+        public string Preco { get; set; }
+    }
+}
--- ApiCatalogoJogos/ViewModel/JogosViewModel.cs
+++ ApiCatalogoJogos/ViewModel/JogosViewModel.cs
@@ -7,5 +7,10 @@ namespace ApiCatalogoJogos.ViewModel
 {
     public class JogosViewModel
     {
+        public Guid Id { get; set; }
+        public string Nome { get; set; }
+        public string Produtora { get; set; }
+        public double Preco { get; set; }
+
     }
 }
