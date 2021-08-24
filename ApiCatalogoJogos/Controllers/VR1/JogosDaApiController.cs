6aae2f3143ddd5059348a6298df5d44ecc6d50ff
douglashassislima <douglashenriquedeassis@gmail.com>
2021-08-24 09:37:23 -0300

    Adicionando comandos dentro do Controller JogosDaApi

--- ApiCatalogoJogos/Controllers/VR1/JogosDaApiController.cs
+++ ApiCatalogoJogos/Controllers/VR1/JogosDaApiController.cs
@@ -7,9 +7,46 @@ using System.Threading.Tasks;
 
 namespace ApiCatalogoJogos.Controllers.VR1
 {
-    [Route("api/[controller]")]
+    [Route("api/VR1[controller]")]
     [ApiController]
     public class JogosDaApiController : ControllerBase
     {
+        [HttpGet]
+        public async Task<ActionResult<List<object>>> Obter()
+        {
+            return Ok();
+        }
+
+        [HttpGet("{idJogo:guid}")]
+        public async Task<ActionResult<List<object>>> Obter(Guid idJogo)
+        {
+            return Ok();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<object>> InserirJogo(object jogo)
+        {
+            return Ok();
+        }
+
+        [HttpPut("{idJogo}")]
+        public async Task<ActionResult> AtualizarJogo(Guid idJogo, object jogo)
+        {
+            return Ok();
+        }
+
+        [HttpPatch("{idJogo}/preco/{preco:double}")]
+        public async Task<ActionResult> AtualizarJogo(Guid idJogo, double preco)
+        {
+            return Ok();
+        }
+
+        [HttpDelete]
+        public async Task<ActionResult> ApagarJogo(Guid idJogo)
+        {
+            return Ok();
+        }
+
+
     }
 }
