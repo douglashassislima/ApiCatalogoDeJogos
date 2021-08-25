bdc86eae148f5e33d2d92e7c7e1d6e38f05a6abb
douglashassislima <douglashenriquedeassis@gmail.com>
2021-08-25 00:46:06 -0300

    Resolução de erros

--- ApiCatalogoJogos/Controllers/VR1/CicloDeVidaController.cs
+++ ApiCatalogoJogos/Controllers/VR1/CicloDeVidaController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace ApiCatalogoJogos.Controllers.VR1
--- ApiCatalogoJogos/Startup.cs
+++ ApiCatalogoJogos/Startup.cs
@@ -1,3 +1,4 @@
+using ApiCatalogoJogos.Controllers.VR1;
 using ApiCatalogoJogos.Repositories;
 using ApiCatalogoJogos.Services;
 using Microsoft.AspNetCore.Builder;
@@ -11,7 +12,9 @@ using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 
 namespace ApiCatalogoJogos
@@ -29,12 +32,28 @@ namespace ApiCatalogoJogos
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddScoped<IJogoService, JogoService>();
-            services.AddScoped<IJogoRepository, JogoSqlServerRepository>();
+            //services.AddScoped<IJogoRepository, JogoSqlServerRepository>();
+
+            services.AddScoped<IJogoService, JogoService>();
+            services.AddScoped<IJogoRepository, JogoRepository>();
+
+            #region CicloDeVida
+
+            services.AddSingleton<IExemploSingleton, ExemploCicloDeVida>();
+            services.AddScoped<IExemploScoped, ExemploCicloDeVida>();
+            services.AddTransient<IExemploTransient, ExemploCicloDeVida>();
+
+            #endregion
+
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "ApiCatalogoJogos", Version = "v1" });
-            });
+                c.SwaggerDoc("VR1", new OpenApiInfo { Title = "ApiCatalogoJogos", Version = "VR1" });
+                c.SwaggerDoc("VR1", new OpenApiInfo { Title = "CatalogoJogosAPI", Version = "VR1" });
+                var basePath = AppDomain.CurrentDomain.BaseDirectory;
+                var fileName = typeof(Startup).GetTypeInfo().Assembly.GetName().Name + ".xml";
+                c.IncludeXmlComments(Path.Combine(basePath, fileName));
+        });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -44,7 +63,7 @@ namespace ApiCatalogoJogos
             {
                 app.UseDeveloperExceptionPage();
                 app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "ApiCatalogoJogos v1"));
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/VR1/swagger.json", "ApiCatalogoJogos VR1"));
             }
 
             app.UseHttpsRedirection();
