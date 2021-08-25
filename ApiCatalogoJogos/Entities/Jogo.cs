6853784f3a3a41ee543449ba629431b8cdb6d750
douglashassislima <douglashenriquedeassis@gmail.com>
2021-08-24 22:33:42 -0300

    Estabelecendo comandos no JogoService

--- ApiCatalogoJogos/Entities/Jogo.cs
+++ ApiCatalogoJogos/Entities/Jogo.cs
@@ -7,5 +7,9 @@ namespace ApiCatalogoJogos.Entities
 {
     public class Jogo
     {
+        public Guid Id { get; set; }
+        public string Nome { get; set; }
+        public string Produtora { get; set; }
+        public double Preco { get; set; }
     }
 }
--- ApiCatalogoJogos/Repositories/IJogoRepository.cs
+++ ApiCatalogoJogos/Repositories/IJogoRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ApiCatalogoJogos.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
