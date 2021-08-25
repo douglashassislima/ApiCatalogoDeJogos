82583f815e5711a30005e8b84b06de6b4fb55a8a
douglashassislima <douglashenriquedeassis@gmail.com>
2021-08-25 00:23:41 -0300

    Adicionando comandos básicos de banco de dados Sql Server

--- ApiCatalogoJogos/Startup.cs
+++ ApiCatalogoJogos/Startup.cs
@@ -29,7 +29,7 @@ namespace ApiCatalogoJogos
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddScoped<IJogoService, JogoService>();
-            services.AddScoped<IJogoRepository, JogoRepository>();
+            services.AddScoped<IJogoRepository, JogoSqlServerRepository>();
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
