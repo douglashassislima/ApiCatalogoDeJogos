af71f26425f1e303c9601c2ea0b3f773d6bac662
douglashassislima <douglashenriquedeassis@gmail.com>
2021-08-24 21:45:25 -0300

    Adicionando e definindo comandos a interface IJogoRepository

---
+++ ApiCatalogoJogos/Repositories/IJogoRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiCatalogoJogos.Repositories
+{
+    public interface IJogoRepository
+    {
+        Task<List<Jogo>> Obter(int pagina, int quantidade);
+        Task<Jogo> Obter(Guid id);
+        Task<List<Jogo>> Obter(string nome, string produtora);
+        Task Inserir(Jogo jogo);
+        Task Atualizar(Jogo jogo);
+        Task Remover(Jogo jogo);
+    }
+}
